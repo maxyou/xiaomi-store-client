c20443be9954e9e2b8bc1e6f34a17e4d94ee4479
maxyou <hyyou@foxmail.com>
2018-08-08 21:53:01 +0800

    goods list

--- src/components/GoodsList.vue
+++ src/components/GoodsList.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="goods-list">
     <h1>{{ msg }}</h1>
+
+    <ul>
+      <li v-for="item in goodsdata" :key="item.id">{{item}}</li>
+    </ul>
     
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'GoodsList',
+  mounted(){
+    this.getGoodsList()
+  },
   data () {
     return {
-      goodsList: [],
+      goodsdata: '',
       msg: 'this is goods list page'
     }
+  },
+  methods:{
+    getGoodsList() {
+            axios.get("/goods/list", {
+                params: {
+                    page: 0,
+                    pageSize: 8,
+                    orderFlag: true,
+                    priceLevel: '4'
+                }
+            }).then((res) => {
+              this.goodsdata = res.data
+              console.log(JSON.stringify(res.data))
+            })
+        }
   }
 }
 </script>
