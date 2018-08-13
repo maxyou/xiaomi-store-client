444adcd8fb22f7d637e5b9ae8e407108ea4f4e36
maxyou <hyyou@foxmail.com>
2018-08-13 18:20:03 +0800

    cart list table

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,29 +1,66 @@
 <template>
-  <div class="cart-list">
+<div class="cart-list">
     <h4>{{ msg }}</h4>
+    <el-table ref="multipleTable" :data="tableData3" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55">
+        </el-table-column>
+        <el-table-column prop="date" label="日期" width="120"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+        <el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column>
+    </el-table>
 
-
-
-    
     <router-link to="/cart/address" tag="el-button">goto /cart/address</router-link>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Cart',
-  data () {
-    return {
-      goodsList: [],
-      msg: 'this is cart list'
+    name: 'Cart',
+    methods:{
+      handleSelectionChange(val) {
+        console.log(JSON.stringify(val))
+      }
+    },
+    data() {
+        return {
+            tableData3: [{
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-02',
+                name: '王da虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-08',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-06',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-07',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }],
+            msg: 'this is cart list'
+        }
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.cart-list{
-  background-color: darkgoldenrod;
+.cart-list {
+    background-color: darkgoldenrod;
 }
 </style>
