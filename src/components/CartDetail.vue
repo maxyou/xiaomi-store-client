d597bb11c5b973276107aac794baf2b2a8aa17a7
maxyou <hyyou@foxmail.com>
2018-08-18 20:10:43 +0800

    layout

--- src/components/CartDetail.vue
+++ src/components/CartDetail.vue
@@ -1,25 +1,49 @@
 <template>
-  <div class="cart-detail">
+<div class="cart-detail">
     <h1>{{ msg }}</h1>
-    <router-link to="/cart/completed" tag="el-button">goto /cart/completed</router-link>
-  </div>
+    <div class="cart-next">
+        <router-link class="cart-next-goto" to="/cart/completed" tag="a">goto /cart/completed</router-link>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Cart',
-  data () {
-    return {
-      goodsList: [],
-      msg: 'this is cart detail'
+    name: 'Cart',
+    data() {
+        return {
+            goodsList: [],
+            msg: 'this is cart detail'
+        }
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.cart-detail{
-  background-color: goldenrod;
+.cart-detail {
+    background-color: goldenrod;
+}
+
+
+.cart-next {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    /* background-color: aqua; */
+}
+
+.cart-next-goto {
+    /* margin-left: 20px; */
+    padding-left: 20px;
+    padding-right: 20px;
+    background-color: darkgrey;
+    color: white;
+    text-decoration: none;
+    /* height: 100%; */
+    /* display: block; */
+    line-height: 50px;
 }
 </style>
